813dbbb700775b9570401f395cf7a07af36a8566
Jose Laruta <eduardo.laruta@gmail.com>
2026-05-12 20:03:34 -0400

    intro python 1

---
+++ 1._intro_python/control_flow.py
@@ -0,0 +1,8 @@
+edad = int(input("ingrese su edad: "))
+
+if edad < 13:
+    print("niño")
+elif edad < 18:
+    print("adolescente")
+else:
+    print("adulto")
---
+++ 1._intro_python/hello.py
@@ -0,0 +1,12 @@
+edad = 19
+estatura = 1.9
+nombre = "juan"
+
+
+print(type(edad), type(estatura), type(nombre))
+
+print(f"hola, soy {nombre}, tengo {edad} y mido {estatura}m")
+
+print(8 + 9)
+print(edad * 10)
+print(estatura / 2)
